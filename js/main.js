5a48f3de40cac1ba2e1d46791e2e78ffccfd8974
Habibur Rahman <habibrajbd24@gmail.com>
2022-12-09 00:35:41 +0600

    home and channels secton basic design complete

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
 
+// sport channels 
 
-const ch = {
-  tSports:
-    "https://sports.bioscopelive.com/hls/anonymous/aPSmLQmRA6ljEuSmgVXCwQ/1670016518/tsports_mid/index.m3u8",
-  adSports1:
-    "https://admdn1.cdn.mangomolo.com/adsports1/smil:adsports1.stream.smil/chunklist_b450000_t64MzYwcA==.m3u8",
-  dubaiSports1:
-    "https://dmitnthvll.cdn.mangomolo.com/dubaisports/smil:dubaisports.smil/chunklist_b850000.m3u8",
-  dubaiSports3:
-    "https://dmitwlvvll.cdn.mangomolo.com/dubaisportshd5/smil:dubaisportshd5.smil/chunklist_b1300000.m3u8",
-  fightNetwork: "https://d12a2vxqkkh1bo.cloudfront.net/hls/360p/playlist.m3u8",
-  tst: "https://t5.switchcast2.com:999/hls/t8.m3u8?md5=nYSS1ohkF37JP7NSQgJ4DQ&expires=1670024236",
-  gtv: "http://tv.maguraplex.net:8000/live/gazitvhd1/index.m3u8",
+const sports = {
+    adSports1:
+     "https://admdn1.cdn.mangomolo.com/adsports1/smil:adsports1.stream.smil/chunklist_b450000_t64MzYwcA==.m3u8",
+   dubaiSports1:
+     "https://dmitnthvll.cdn.mangomolo.com/dubaisports/smil:dubaisports.smil/chunklist_b850000.m3u8",
+   dubaiSports3:
+     "https://dmitwlvvll.cdn.mangomolo.com/dubaisportshd5/smil:dubaisportshd5.smil/chunklist_b1300000.m3u8",
+   fightNetwork: "https://d12a2vxqkkh1bo.cloudfront.net/hls/360p/playlist.m3u8",
+   tudn: "http://zaintv.live:8080/live/king/1234/1787.m3u8",
 };
+ 
+
+
+
+
+
+//  <button type="button"  class="btn btn-primary btn-lg"  >
+//  Abu Dhabi 1
+// </button>
 
 
 
@@ -38,3 +45,21 @@ function tvRemote(url) {
 	})
 } 
 
+
+// horizontal scroll 
+
+(function () {
+	function scrolH(e) {
+		e.preventDefault();
+		e = window.event || e;
+		let delta = Math.max(-1, Math.min(1, (e.wheelDelta || e.detail)));
+		document.querySelector('.cardsRow').scrollLeft -= (delta * 40);
+	}	
+	if (document.querySelector('.cardsRow').addEventListener) {
+		document.querySelector('.cardsRow').addEventListener('mousewheel', scrolH, false);
+		document.querySelector('.cardsRow').addEventListener('DOMMouseScroll', scrolH, false);
+	}
+}
+)()
+	
+	
