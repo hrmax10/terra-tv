dff683efa83ceecbd68747bbc4f69b36f2de5fca
Habibur Rahman <habibrajbd24@gmail.com>
2022-12-08 15:02:15 +0600

    modal design and functionality added

--- js/main.js
+++ js/main.js
@@ -14,25 +14,6 @@ const ch = {
   gtv: "http://tv.maguraplex.net:8000/live/gazitvhd1/index.m3u8",
 };
 
-// const player = new Plyr('#player');
-
-// function vdoReq(url) {
-
-
-//   player.source = {
-//     type: 'video',
-//     title: 'Example title',
-//     sources: [
-//       {
-//         src: 'https://admdn1.cdn.mangomolo.com/adsports1/smil:adsports1.stream.smil/playlist.m3u8',
-//         type: 'application/x-mpegURL',
-//         size: 720,
-//       },
-//     ],
-    
-//   };
-
-// }
 
 
 function tvRemote(url) {
@@ -40,33 +21,20 @@ function tvRemote(url) {
   const source = url;
 	const video = document.querySelector('video');
 	
-	// For more options see: https://github.com/sampotts/plyr/#options
-	// captions.update is required for captions to work with hls.js
 	const player = new Plyr(video, {captions: {active: true, update: true, language: 'en'}});
 	
 	if (!Hls.isSupported()) {
 		video.src = source;
 	} else {
-		// For more Hls.js options, see https://github.com/dailymotion/hls.js
 		const hls = new Hls();
 		hls.loadSource(source);
 		hls.attachMedia(video);
 		window.hls = hls;
-		
-		// Handle changing captions
-		player.on('languagechange', () => {
-			// Caption support is still flaky. See: https://github.com/sampotts/plyr/issues/994
-			setTimeout(() => hls.subtitleTrack = player.currentTrack, 50);
-		});
 	}
-	
-	// Expose player so it can be used from the console
-	window.player = player;
 
+	// close modal and stop the playback
+	 document.getElementById('modalClose').addEventListener('click', () => { 
+		 player.stop();
+	})
+} 
 
-}
-
-
-function stopIt() {
-  player.stop();
-}
